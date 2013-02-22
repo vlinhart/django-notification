cd431302761342077f08492f1dc9c32830f8b608
Benoit Bar <bar.benoit@gmail.com>
2013-02-22 11:56:57 +0100

    adding settings to limit notice select in notice context processor

--- notification/__init__.py
+++ notification/__init__.py
@@ -1,1 +1,6 @@
 __version__ = "1.0"
+
+from django.conf import settings
+
+
+settings.NOTIFICATION_NOTICE_PROCESSOR_LIMIT = getattr(settings,  "NOTIFICATION_NOTICE_PROCESSOR_LIMIT", 10)
--- notification/context_processors.py
+++ notification/context_processors.py
@@ -1,9 +1,12 @@
 from notification.models import Notice
+from django.conf import settings
 
 
 def notice(request):
     if request.user.is_authenticated():
-        notices = Notice.objects.notices_for(request.user)
-        count_noticies = Notice.objects.unseen_count_for(request.user)
+        notices = Notice.objects.notices_for(request.user)[:settings.NOTIFICATION_NOTICE_PROCESSOR_LIMIT]
+        count_noticies = 0
+        for notice in notices:
+            count_noticies += notice.unseen
         return {'NOTICIES': notices, 'COUNT_NOTICIES': count_noticies}
     return {}
