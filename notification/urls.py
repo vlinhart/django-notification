558cb1f8b2f2ed050edec74a1876ed98e138a061
vlinhart <vladimir.linhart@gmail.com>
2013-12-21 20:45:33 +0100

    update for django 1.6, backward compatible

--- notification/urls.py
+++ notification/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls.defaults import patterns, url
+try:
+    from django.conf.urls.defaults import patterns, url
+except ImportError:
+    from django.conf.urls import patterns, url
+
 
 urlpatterns = patterns("notification.views",
     url(r"^settings/$", 'notice_settings', name="notification_notice_settings"),
